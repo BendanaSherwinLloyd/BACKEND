f1d8763b917958d1fc2d6bb188c42d76cb15096a
BendanaSherwinLloyd <23-16733@g.batstate-u.edu.ph>
2025-11-19 11:47:01 +0700

    Login

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,12 +1,22 @@
 import pool from '../models/db.js';
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const getUser = async (id) =>{
+    if(parseInt(id) === NaN){
+        throw new Error('Invalid id');
+    }
+
+    const [user] = await pool.query('SELECT * FROM tbluser WHERE id = ?', [id]);
+    return user;
+}
 
 export const createUser = async (email, password) =>{
     if(email === ''){
-        throw new Error ("Invalid email");
+        throw new Error ('Invalid email');
     }
-    
+
     if(!validator.isEmail(email)){
         throw new Error('Invalid email format');
     }
@@ -16,16 +26,18 @@ export const createUser = async (email, password) =>{
         [email]
     )
 
-    if(user.length > 0){
-        throw new Error('An account is already created with that email')
-    }
-
     if(password === ''){
-        throw new Error('Invalid password') 
+        throw new Error ('Invalid password');
     }
 
     if(!validator.isStrongPassword(password)){
         throw new Error('Password too weak.');
+    }
+
+    if(user.length === 1){
+        const error = new Error(`The email ${email} is already used.`)
+        error.statusCode = 400;
+        throw error;
     }
 
     const salt = bcrypt.genSaltSync(10);
@@ -37,4 +49,24 @@ export const createUser = async (email, password) =>{
     )
 
     return newUser.insertId;
+}
+
+export const login = async (email, password) =>{
+    if(email === '' || password === ''){
+        throw new Error('Email and Password is required');
+    }
+
+    const [user] = await pool.query("SELECT * FROM tbluser WHERE email = ?", [email]);
+    if(user.length === 0){
+        throw new Error(`An account with email: ${email} does not exist.`);
+    }
+
+    if(!bcrypt.compareSync(password, user[0].password)){
+        throw new Error('Incorrect password');
+    }
+
+    //generate token
+    const token = jwt.sign({id: user[0].id}, process.env.SECRET, {expiresIn: '1d'});
+
+    return token;
 }
